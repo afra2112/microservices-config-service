40bad826a7412042dbfec55676e83884cb0ef7e3
afra65069@gmail.com <andres.ramirez01@globant.com>
2026-03-08 10:49:32 -0500

    first commit

---
+++ src/main/java/org/microservice/configserverservice/ConfigserverServiceApplication.java
@@ -0,0 +1,15 @@
+package org.microservice.configserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/microservice/configserverservice/ConfigserverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microservice.configserverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
